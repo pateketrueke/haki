6cc0e111a0ca5672625573ba4a2ec818b71cdc51
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-11 23:51:29 -0600

    Lean CLI

--- bin/lib/util.js
+++ bin/lib/util.js
@@ -9,7 +9,21 @@ function echo() {
   process.stdout.write(_slice.call(arguments).join(''));
 }
 
+function merge(target) {
+  _slice.call(arguments, 1).forEach(source => {
+    Object.keys(source).forEach(key => {
+      /* istanbul ignore else */
+      if (typeof target[key] === 'undefined') {
+        target[key] = source[key];
+      }
+    });
+  });
+
+  return target;
+}
+
 module.exports = {
   die,
   echo,
+  merge,
 };
