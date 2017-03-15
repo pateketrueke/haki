9ad08836c5ac2ce4084de4fee2264fd673a0f6e8
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-15 14:51:06 -0600

    Fixed clone

--- bin/Hakifile.js
+++ bin/Hakifile.js
@@ -1,6 +1,5 @@
 const HAKI_HINTS = [
   '`haki` without arguments will run the default generator',
-  '`haki init` to make a new Hakifile.js here',
   '`haki --help` to display all available generators',
   '`haki --quiet` will supress status output from logging',
   '`haki --force` will overwrite any files even if they exists',
@@ -18,31 +17,11 @@ const HAKI_TXT = `
   Try: ${HAKI_HINTS[Math.floor(Math.random() * HAKI_HINTS.length)]}
 `;
 
-const HAKIFILE_TXT = `module.exports = haki => {
-  haki.setGenerator('default', {
-    description: 'Default generator',
-    abortOnFail: true,
-    actions() {
-      throw new Error('Not implemented!');
-    },
-  });
-};
-`;
-
 module.exports = haki => {
   haki.setGenerator('default', {
     description: 'Print a huge banner ;-)',
     actions() {
       process.stdout.write(`${HAKI_TXT}\n`);
     }
-  });
-
-  haki.setGenerator('init', {
-    description: 'Create a new Hakifile.js',
-    actions: [{
-      type: 'add',
-      dest: 'Hakifile.js',
-      content: HAKIFILE_TXT,
-    }]
   });
 };
