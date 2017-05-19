7c18e55c6610ae943c13efe147717f36fa25fb0d
Alvaro Cabrera <pateketrueke@gmail.com>
2017-05-19 16:32:15 -0500

    Quick impl for local gists; fix

--- bin/Hakifile.js
+++ bin/Hakifile.js
@@ -1,9 +1,15 @@
 const HAKI_HINTS = [
   '`haki` without arguments will run the default generator',
+  '`haki --gist` help you to manage your haki-gists',
   '`haki --help` to display all available generators',
   '`haki --quiet` will supress status output from logging',
   '`haki --force` will overwrite any files even if they exists',
   '`haki --debug` print the stack trace whenever an error occurs',
+  '`haki --no-add` will skip from writing new files',
+  '`haki --no-copy` will skip from copying new files',
+  '`haki --no-exec` will skip from executing commands',
+  '`haki --no-clone` will skip from cloning repositories',
+  '`haki --no-install` will skip from installing dependencies',
 ];
 
 const HAKI_TXT = `
